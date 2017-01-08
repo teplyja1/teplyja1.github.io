564ddddc921f26ef9e436e0a539c640328c1faea
jan.teply <teply.honza@gmail.com>
2017-01-08 11:00:09 +0100

    second commit

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/assets/img/login-logo.png","f44801c098da1b1c17402a0e30149395"],["/assets/svg/more.svg","04f42f63d9a98931ad03a925c210e3c8"],["/css/main.css","ee2f4119c4d52bb877bab01713a67519"],["/index.html","e905efb1cd2577274a9984cafb9b523f"],["/js/app.js","420a0aa9516368bd43b5df15e6cb43ec"],["/js/shims.js","31f2ea72266db5aaaa958cd509cc62b5"]];
+var precacheConfig = [["/assets/img/login-logo.png","f44801c098da1b1c17402a0e30149395"],["/assets/svg/more.svg","04f42f63d9a98931ad03a925c210e3c8"],["/css/main.css","ee2f4119c4d52bb877bab01713a67519"],["/index.html","e905efb1cd2577274a9984cafb9b523f"],["/js/app.js","b34f0d8609603a4deb1d017c17781a47"],["/js/shims.js","31f2ea72266db5aaaa958cd509cc62b5"]];
 var cacheName = 'sw-precache-v2--' + (self.registration ? self.registration.scope : '');
 
 
